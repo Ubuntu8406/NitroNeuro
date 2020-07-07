8798451c0c89d80b92e4144d85c7558714397133
UEBUNTU <toropov-06@list.ru>
2020-07-07 13:34:12 +0300

    Updated Neuron.java

---
+++ src/com/u84buntu/intefaces/Neuron.java
@@ -0,0 +1,8 @@
+package com.u84buntu.intefaces;
+
+import java.util.Random;
+
+public interface Neuron {
+    void Input(float[] inputs);
+    void Output();
+}
--- src/com/u84buntu/neurons/InputNeuron.java
+++
@@ -1,7 +0,0 @@
-package com.u84buntu.neurons;
-
-import java.util.Random;
-
-public interface InputNeuron {
-    void Input(float[] inputs);
-}
